8aa989743fe872dea356a5f93af44a4f98177b94
Emilio Lopez <lopez.emilio@unah.hn>
2026-06-11 22:06:11 -0600

    Prueba de subido

--- ExmanenPOO/Program.cs
+++ ExmanenPOO/Program.cs
@@ -1,1 +1,6 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// using System.Formats.Asn1;
+// using System.Net.Http.Headers;
+// using Proyecto.Clases;
+// using Proyecto.Condicional;
+// using Proyecto.Ciclos;
+Console.WriteLine("Hello World!mario");
